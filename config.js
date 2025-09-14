eae0a14584b82bc448472944053148920a38d61b
Tumelo <tumelontsilo@gmail.com>
2025-09-14 20:07:46 +0200

    Updated the page and added navbar

---
+++ config.js
@@ -0,0 +1,9 @@
+tailswind.config = {
+    theme: {
+        extend: {
+            fontFamily: {
+                agbalumo: ['Agbalumo', 'Playfair Display SC', 'sans-serif']
+            }
+        }
+    }
+}
